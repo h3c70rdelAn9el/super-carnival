e7ca3e80e5ad3665d8132b581121baca0c45b25e
Hector del Angel <hectordelangel@gmail.com>
2021-04-29 16:57:24 -0700

    add home page, parallax, images

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Photo by <a href="https://unsplash.com/@all_who_wander?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kinga Cichewicz</a> on <a href="https://unsplash.com/s/photos/photographer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+  </div>
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,123 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1 class="text-red-600">Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="relative h-screen">
+      <div class="absolute top-72 left-0 right-0">
+        <div class="flex flex-col opacity-75">
+          <div
+            class="bg-pink-400 text-center  mx-auto px-20 rounded-md shadow-lg py-4"
+          >
+            <h1 class="text-black text-4xl p-5">Erin Black</h1>
+            <p class="text-2xl">Portrait Studio</p>
+          </div>
+        </div>
+      </div>
+      <parallax
+        :speed-factor="0.4"
+        breakpoint="(min-width: 80px)"
+        class="p-container"
+      >
+        <img
+          class="h-full opacity-80"
+          src="../assets/images/sofia-hernandez-8cCfjxR8KTw-unsplash.jpg"
+        />
+      </parallax>
+    </div>
+    <section class="h-full flex flex-col mx-auto sm:flex-row">
+      <div class="text-center mx-auto w-1/2 p-4 ">
+        <div>
+          <h2 class="text-xl mb-4">photography</h2>
+          <h2>At your service</h2>
+        </div>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+          atque impedit obcaecati commodi fugit ipsa.
+        </p>
+        <hr class="border border-black" />
+        <p class="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <button
+          class="bg-coral p-2 rounded-md shadow-lg hover:bg-gray-500 hover:text-gray-200"
+        >
+          Book Now
+        </button>
+      </div>
+      <div
+        class="text-center mb-2 border border-black rounded-md mx-auto w-1/2"
+      >
+        <g-image
+          src="../assets/images/kinga-cichewicz-HHw9lc0ogIs-unsplash.jpg"
+          class="h-full rounded-md object-cover p-4"
+        ></g-image>
+      </div>
+    </section>
+    <section
+      class="flex items-center justify-center h-screen bg-fixed bg-center bg-cover parallax-bottom-container relative opacity-75"
+      style="background-image: ('../assets/images/vitor-pinto-3bAJYaK37yE-unsplash.jpg')"
+    >
+      <div
+        class="p-3 w-1/2 text-black bg-purple-300 bg-opacity-60 rounded-xl shadow-md hover:bg-gray-500 hover:bg-opacity-80"
+      >
+        <g-link to="/portfolio" class="mx-auto ">
+          <div>
+            <h2 class="text-center text-2xl mb-2">Portfolio</h2>
+            <p class="text-sm p-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consectetur, accusantium numquam quia praesentium ratione neque
+              soluta. Doloribus, voluptates iusto! Numquam.
+            </p>
+          </div>
+        </g-link>
+      </div>
+    </section>
+    <section class="bg-gray-200 p-5 w-full h-full">
+      <div class="m-4 text-center">
+        <p class="m-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel aut,
+          repudiandae nisi ullam ratione tempora ea illum impedit quidem libero.
+        </p>
+        <hr class="border border-black" />
+        <p class="m-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+          cupiditate sunt voluptate vero suscipit. Porro id natus ad!
+          Perferendis aut laudantium quibusdam commodi numquam nemo sed, maiores
+          voluptate consequatur unde delectus temporibus tenetur vel
+          necessitatibus aliquid cumque pariatur maxime! Libero!
+        </p>
+      </div>
+    </section>
   </Layout>
 </template>
 
 <script>
+import Parallax from 'vue-parallaxy'
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
+  components: {
+    Parallax,
+  },
 }
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+.parallax-bottom-container {
+  position: relative;
+  background-image: url('../assets/images/vitor-pinto-3bAJYaK37yE-unsplash.jpg');
+}
+
+.parallax-bottom-title {
+  position: absolute;
+  top: 30%;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+.p-container {
+  height: 100vh !important;
+}
+
+.g-image {
+  border-radius: 20px;
 }
 </style>
