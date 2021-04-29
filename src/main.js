919b1ece5ac846e930245be1faa0ac7462be0ae6
Hector del Angel <hectordelangel@gmail.com>
2021-04-29 10:27:08 -0700

    add v-lightbox dependency and portfolio page

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ require('~/assets/css/main.css')
 import DefaultLayout from '~/layouts/Default.vue'
 import '~/assets/css/main.css'
 
+import lightbox from 'vlightbox'
+
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(lightbox)
 }
---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,46 @@
+<template>
+  <Layout>
+    <div class="bg-gray-300 h-full mt-8">
+      <div class="mb-4 text-center text-2xl pt-4">Portfolio</div>
+      <lightbox :images="images" title="" class=""></lightbox>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        {
+          src: require('../assets/images/1.jpg'),
+          caption: 'Image 1',
+        },
+        {
+          src: require('../assets/images/foto-pettine-IfjHaIoAoqE-unsplash.jpg'),
+          caption: 'Le Image 2',
+        },
+        {
+          src: require('../assets/images/sofia-hernandez-8cCfjxR8KTw-unsplash.jpg'),
+          caption: 'le Image tres',
+        },
+        {
+          src: require('../assets/images/nathan-dumlao-5BB_atDT4oA-unsplash.jpg'),
+          caption: 'le Image tres',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.lightbox-overlay {
+  margin-top: 36px;
+  
+}
+
+.holder {
+  padding-top: 50px;
+}
+</style>
