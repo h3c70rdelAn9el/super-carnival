eb6e25218a1f0666e40383fe5f528b8cbffd6762
Hector del Angel <hectordelangel@gmail.com>
2021-05-10 15:51:33 -0700

    clean up some empty divs on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,16 +3,6 @@
     <div>
       <NavbarModal />
     </div>
-    <div class="text-center text-3xl text-gray-800">
-      <strong>
-        <g-link to="/">
-        <!-- <img src="../assets/images/LogoMakr-2bCkpf.png" alt="logo" class="w-32"> -->
-          <!-- <g-image scr="../assets/images/LogoMakr-2bCkpf.png"></g-image> -->
-        </g-link>
-      </strong>
-    </div>
-    <div class="w-10">
-    </div>
   </nav>
 </template>
 
