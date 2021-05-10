4886adbb7a6224f44e20cfe0c9c3798bb5d04221
Hector del Angel <hectordelangel@gmail.com>
2021-05-10 15:17:32 -0700

    fix parallax div height.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
-  <Layout>
-    <div class="relative mt-20 pt-2">
-      <div class="absolute top-60 left-0 right-0">
+  <SolidLayout>
+    <div class="relative h-80 pt-2">
+      <div class="absolute top-2/3 sm:top-40 left-0 right-0">
         <div class="flex flex-col opacity-75">
           <div
             class="bg-pink-400 text-center  mx-auto -mt-32 sm:-mt-12 px-10 rounded-md shadow-lg py-4"
@@ -11,30 +11,39 @@
         </div>
       </div>
       <parallax
-        :speed-factor="0.4"
+        :speed-factor="0.6"
         breakpoint="(min-width: 80px)"
         class="p-container"
       >
-        <img
-          class="h-32 opacity-80"
-          src="../assets/images/field.jpg"
-        />
+        <g-image class="opacity-80" src="../assets/images/field.jpg" />
       </parallax>
-      <section class="text-center bg-gray-200 px-14 pb-2">
-        <h2 class="text-2xl font-shadows tracking-wider pt-2 mb-3">About Me</h2>
-        <div class="mx-16">
-
-          <p class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam nostrum esse a laudantium repellat culpa et nemo possimus mollitia quas?</p>
-          <hr class="border border-red-900">
-          <p class="my-3">Numquam ut modi, debitis error est ea quae nulla laudantium ad quibusdam, vitae totam quis! Maiores non quis dolorem cum minus pariatur!</p>
-          <hr class="border border-red-900">
-          <p class="my-3">Possimus, eligendi, cumque eum labore fuga reiciendis placeat consectetur sed obcaecati rerum accusamus recusandae error natus id dolore excepturi minus dolorum dignissimos nesciunt molestias dolor fugit architecto ipsa magnam? Sit possimus maxime corrupti omnis adipisci quas?</p>
-        </div>
-      </section>
     </div>
-  </Layout>
+    <section class="text-center bg-gray-200 px-14 pb-2">
+      <h2 class="text-2xl font-shadows tracking-wider pt-2 mb-3">About Me</h2>
+      <div class="mx-16">
+        <p class="mb-3">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam nostrum
+          esse a laudantium repellat culpa et nemo possimus mollitia quas?
+        </p>
+        <hr class="border border-red-900" />
+        <p class="my-3">
+          Numquam ut modi, debitis error est ea quae nulla laudantium ad
+          quibusdam, vitae totam quis! Maiores non quis dolorem cum minus
+          pariatur!
+        </p>
+        <hr class="border border-red-900" />
+        <p class="my-3">
+          Possimus, eligendi, cumque eum labore fuga reiciendis placeat
+          consectetur sed obcaecati rerum accusamus recusandae error natus id
+          dolore excepturi minus dolorum dignissimos nesciunt molestias dolor
+          fugit architecto ipsa magnam? Sit possimus maxime corrupti omnis
+          adipisci quas?
+        </p>
+      </div>
+    </section>
+    <!-- </div> -->
+  </SolidLayout>
 </template>
-
 
 <script>
 import Parallax from 'vue-parallaxy'
@@ -47,7 +56,7 @@ export default {
 </script>
 
 <style>
-.p-container {
-  height: 25vh!important;
+.p-container img {
+  height: 500px !important;
 }
 </style>
